ad59757fd8392f757259a1f9db17fbd2cd22d323
BrunoGiumprini <106162844+BrunoGiumprini@users.noreply.github.com>
2022-06-27 00:47:56 -0300

    Solucionar el problema de la conexion

--- Prog/validacion.php
+++ Prog/validacion.php
@@ -6,30 +6,34 @@ $bd = "registro_prueba";
 
 $conn = new mysqli($servername, $username, $password, $bd);
 
-$name = $_POST['nombre'];
-$apellido = $_POST['apellido'];
-$mail = $_POST['email'];
-$date = $_POST['fechanacimiento'];
-$pssw = $_POST['password'];
+if (mysqli_connect_errno()) {
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
+    exit();
+}
 
+$name = $_POST['nombre'];
+$apellido = mysqli_real_escape_string($conn, $_POST['apellido']);
+$mail = mysqli_real_escape_string($conn, $_POST['email']);
+$date = date($_POST['fechanacimiento']);
+$pssw = mysqli_real_escape_string($conn, $_POST['password']);
 
 if (isset($_POST['submit'])) {
     if (
-        strlen($_POST['nombre']) > 0 &&
-        strlen($_POST['apellido']) > 0 &&
-        strlen($_POST['email']) > 0 &&
-        strlen($_POST['fechanacimiento']) > 0 &&
-        strlen($_POST['password']) > 0
+        !empty($name) &&
+        !empty($apellido) &&
+        !empty($mail) &&
+        !empty($date) &&
+        !empty($pssw)
     ) {
-
         $sql = "INSERT INTO `usuarios`(`nombre`, `apellido`, `email`, `fnac`, `contraseña`) VALUES ('$name','$apellido','$mail','$date','$pssw')";
 
         if (mysqli_query($conn, $sql)) {
-            echo "New record created successfully";
+            echo "Registrado exitosamente!";
+            header("Location: ../index.html");
         } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-    }
+    } else echo 'error';
 }
 
 ?>
