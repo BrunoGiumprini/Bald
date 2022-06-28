8ffb6b387672d1dad54f3cbb2202da50aa671a15
BrunoGiumprini <106162844+BrunoGiumprini@users.noreply.github.com>
2022-06-27 21:39:13 -0300

    enlazar registro con la api

--- Diseño Web/html/registro.php
+++ Diseño Web/html/registro.php
@@ -1,48 +1,32 @@
 <html lang="es">
-
 <head>
-
-
-    <meta charset="utf-8">
-
+    <meta charset = "utf-8">
     <title>Registro de Usuario</title>
-
-    <link rel="stylesheet" href="../css/estiloregistro.css">
-
+    <link rel = "stylesheet" href = "../css/estiloregistro.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;500;600&display=swap" rel="stylesheet">
-
 </head>
 
 <body>
-
-    <form method="post">
+    <form action="../php/validacion.php" method="post">
         <h3>Registro</h3>
-
         <label for="nombre"> Nombre </label>
         <input type="text" name="nombre" placeholder="Ingresa tu nombre" maxlength="30">
-
-        <label for="aepllido"> Apellido </label>
+        
+        <label for="apellido"> Apellido: </label>
         <input type="text" name="apellido" placeholder="Ingresa tu apellido" maxlength="30">
 
         <label for="email"> Email: </label>
-        <input type="text" name="email" maxlength="30" placeholder="Ingresa un email">
+        <input type="text" name="email" maxlength="30" placeholder="Ingresa un email"> 
 
         <label for="nacimiento"> Fecha de Nacimiento: </label>
-        <input type="date" name="fechanacimiento">
+        <input type="date" name="fechanacimiento"> 
 
         <label for="password"> Contraseña </label>
-        <input type="password" name="password" placeholder="Ingresa una contrseña" maxlength="30">
+        <input type="password" name="password" placeholder="Ingresa una contraseña" maxlength="30">
 
-        <button name="submit">Siguente</button>
-
+        <input type="submit" name="submit" />
     </form>
-
-    <?php
-    include('validacion.php');
-    ?>
-
 </body>
-
 </html>
