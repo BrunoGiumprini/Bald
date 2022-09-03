b31e1fe5dbe73898c47f910f5112879121055291
MateSiu7 <107948144+MateSiu7@users.noreply.github.com>
2022-09-02 23:44:04 -0300

    Segunda entrega parte final

--- Prog/registro_form.php
+++ Prog/registro_form.php
@@ -6,9 +6,9 @@ include("conex.php");
 $_SESSION['errorMsg'] = '';
 
 if(empty($_POST['nom_usuario']) || empty($_POST['email']) || empty($_POST['fechanacimiento']) || empty($_POST['password'])) {
-        $_SESSION['errorMsg'] = "Todos los campos son obligatorios!";
-        header("Location: ../Diseño Web/html/registro.php");
-} 
+    $_SESSION['errorMsg'] = "Todos los campos son obligatorios!";
+    header("Location: ../Diseño Web/html/registro.php");
+}
 else {
     $user = mysqli_real_escape_string($conn, $_POST['nom_usuario']);
     $mail = mysqli_real_escape_string($conn, $_POST['email']);
